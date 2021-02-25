08bcd1a79d930249a42b3e270d5c416f8e600010
rany <rany97@qq.com>
2021-02-25 12:28:05 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -1,46 +1,27 @@
-// Leetcode 1004
+// Leetcode
+/* #include <algorithm> */
 #include <stdio.h>
-#include <deque>
-#include <vector>
-using namespace std;
+/* #include <vector> */
+/* using namespace std; */
 
-int longestSubarray(vector<int>& nums, int limit)
-{
-    int N=nums.size();
-    int ans=0;
-    int left=0;
-    int right=0;
-    int curNums;
-    deque<int> minQ, maxQ;
-    while(right<N){
-        curNums=nums[right++];
-        while(!minQ.empty() && minQ.back()>curNums){
-            minQ.pop_back();
-        }
-        minQ.push_back(curNums);
-        while(!maxQ.empty() && maxQ.back()<curNums){
-            maxQ.pop_back();
-        }
-        maxQ.push_back(curNums);
-
-        while(maxQ.front()-minQ.front() > limit){
-            curNums=nums[left++];
-            if(curNums==minQ.front()){
-                minQ.pop_front();
-            }
-            if(curNums==maxQ.front()){
-                maxQ.pop_front();
-            }
-        }
-        ans=max(ans, right-left);
+class A {
+public:
+    A(int i)
+    {
+        printf("Default\n");
     }
-    return ans;
-}
+    A(const A& other)
+    {
+        printf("Left Value\n");
+    }
+    A(A&& other)
+    {
+        printf("Right Value\n");
+    }
+};
 
-int main()
+int main(int argc, char* argv[])
 {
-    vector<int> nums={4,2,2,2,4,4,2,2};
-    int limit=0;
-    printf("%d", longestSubarray(nums, limit));
+    A a=A(A(1));
     return 0;
 }
