7679dd84c796259985872038bf1e2511edb0ead7
rany <rany97@qq.com>
2021-02-25 23:50:17 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -1,27 +1,9 @@
 // Leetcode
-/* #include <algorithm> */
 #include <stdio.h>
-/* #include <vector> */
-/* using namespace std; */
+#include <vector>
+using namespace std;
 
-class A {
-public:
-    A(int i)
-    {
-        printf("Default\n");
-    }
-    A(const A& other)
-    {
-        printf("Left Value\n");
-    }
-    A(A&& other)
-    {
-        printf("Right Value\n");
-    }
-};
-
-int main(int argc, char* argv[])
+int main(void)
 {
-    A a=A(A(1));
     return 0;
 }
