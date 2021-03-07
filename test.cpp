3b9f2502b16ccfdaf7ecdd2953085e6fb569f896
rany <rany97@qq.com>
2021-03-07 22:45:22 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -1,56 +1,50 @@
 // Leetcode 678
+#include <functional>
 #include <stdio.h>
-#include <string>
 #include <vector>
+#include <queue>
 using namespace std;
 
-void init(const string& str, vector<vector<bool>>& pa)
-{
-    int N=str.length();
-    for(int j=1;j<N;++j){
-        for(int i=j-1;i>=0;--i){
-            pa[i][j]=(str[i]==str[j]) && pa[i+1][j-1];
-        }
-    }
+inline bool inbound(int N, int M, int x, int y){
+    return x>=0 && x<N && y>=0 && y<M;
 }
 
-void backtrace(int start, const string& str, vector<vector<bool>>& pa, vector<string>& cur, vector<vector<string>>& ans)
+int kthLargestValue(vector<vector<int>>& matrix, int k)
 {
-    int N=str.length();
-    if(start==N){
-        ans.push_back(cur);
-        return;
-    }
-    for(int end=start;end<N;++end){
-        if(!pa[start][end]){
-            continue;
+    int N=matrix.size();
+    int M=matrix[0].size();
+    priority_queue< int, vector<int>, greater<int>> pq;
+    int count=0;
+    for(int i=0;i<N;++i){
+        for(int j=0;j<M;++j){
+            if(inbound(N, M, i-1, j)){
+                matrix[i][j]^=matrix[i-1][j];
+            }
+            if(inbound(N, M, i, j-1)){
+                matrix[i][j]^=matrix[i][j-1];
+            }
+            if(inbound(N, M, i-1, j-1)){
+                matrix[i][j]^=matrix[i-1][j-1];
+            }
+            if(count<k){
+                pq.push(matrix[i][j]);
+                ++count;
+            }else{
+                if(pq.top()<matrix[i][j]){
+                    pq.pop();
+                    pq.push(matrix[i][j]);
+                }
+            }
         }
-        cur.push_back(str.substr(start,end-start+1));
-        backtrace(end+1, str, pa, cur, ans);
-        cur.pop_back();
     }
-}
-
-vector<vector<string>> partition(string s)
-{
-    int N=s.length();
-    vector<vector<bool>> pa(N,vector<bool>(N,true));
-    init(s, pa);
-    vector<vector<string>> ans;
-    vector<string> cur;
-    backtrace(0, s, pa, cur, ans);
-    return ans;
+    return pq.top();
 }
 
 int main(void)
 {
-    string s="aab";
-    auto ans=partition(s);
-    for(auto& part:ans){
-        for(auto& each:part){
-            printf("%s\t",each.c_str());
-        }
-            printf("\n");
-    }
+    vector<vector<int>> matrix={
+        {5,2},{1,6}
+    };
+    printf("%d",kthLargestValue(matrix, 3));
     return 0;
 }
