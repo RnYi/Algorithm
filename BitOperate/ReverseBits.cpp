3557e83384996bac82f0cc64e8b44f7599b1a97c
rany <rany97@qq.com>
2021-04-15 22:03:24 +0800

    Update

---
+++ BitOperate/ReverseBits.cpp
@@ -0,0 +1,21 @@
+// Leetcode
+#include <algorithm>
+#include <stdio.h>
+#include <stdint.h>
+using namespace std;
+
+uint32_t reverseBits(uint32_t n) 
+{
+    n = (n >> 1 & 0x55555555) | (n & 0x55555555) >> 1;
+    n = (n >> 1 & 0x33333333) | (n & 0x33333333) >> 1;
+    n = (n >> 1 & 0x0f0f0f0f) | (n & 0x0f0f0f0f) >> 1;
+    n = (n >> 1 & 0x00ff00ff) | (n & 0x00ff00ff) >> 1;
+    n = n << 16 | n >> 16;
+
+    return n;
+}
+
+int main(void)
+{
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,10 +1,34 @@
-// Leetcode
 #include <algorithm>
-#include <stdio.h>
+#include <array>
 #include <vector>
+
 using namespace std;
+
+int rob_help(vector<int>& nums, int start, int end)
+{
+    if(start>end){
+        return 0;
+    }
+    array<int, 2> dp{0};
+    for(int i=start;i<=end;++i){
+        int tmp = dp[1];
+        dp[1] = max(dp[0]+nums[i], dp[1]);
+        dp[0] = tmp;
+    }
+    return dp[1];
+}
+
+int rob(vector<int>& nums)
+{
+    int N = nums.size();
+    int ans0 = rob_help(nums, 0, N-2);
+    int ans1 = rob_help(nums, 1, N-1);
+    return max({nums[0], ans0, ans1});
+}
 
 int main(void)
 {
+    vector<int> nums = {2,3,2};
+    printf("rob(nums) = %d\n", rob(nums));
     return 0;
 }
