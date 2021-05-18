aa9c54d0399238c29d74ac8ff35b1d29c6a3a974
rany <rany97@qq.com>
2021-05-18 14:48:02 +0800

    Update

--- BIT/ReversePairs_BIT.cpp
+++ BIT/ReversePairs_BIT.cpp
@@ -1,47 +1,54 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-class BIT{
+class BIT {
     int count;
     vector<int> tree;
-    public:
-        BIT(int n):count(n),tree(n+1,0){}
-        static int lowbit(int num){
-            return num & (-num);
-        }
 
-        int sum(int index){
-            int result = 0;
-            while(index>0){
-                result += tree[index];
-                index -= lowbit(index);
-            }
-            return result;
-        }
+public:
+    BIT(int n)
+        : count(n)
+        , tree(n + 1, 0)
+    {
+    }
+    static int lowbit(int num)
+    {
+        return num & (-num);
+    }
 
-        void add(int index,int num){
-            while(index<=count){
-                tree[index] += num;
-                index += lowbit(index);
-            }
+    int sum(int index)
+    {
+        int result = 0;
+        while (index > 0) {
+            result += tree[index];
+            index -= lowbit(index);
         }
+        return result;
+    }
+
+    void add(int index, int num)
+    {
+        while (index <= count) {
+            tree[index] += num;
+            index += lowbit(index);
+        }
+    }
 };
 
-int reversePairs(vector<int>& nums) 
+int reversePairs(vector<int>& nums)
 {
     int len = nums.size();
     vector<int> tmp = nums;
     sort(tmp.begin(), tmp.end());
-    for(auto& num:nums){
+    for (auto& num : nums) {
         num = lower_bound(tmp.begin(), tmp.end(), num) - tmp.begin() + 1;
     }
 
     int rtv = 0;
     BIT bit_tree(len);
-    for (int i = len - 1; i >= 0; --i)
-    {
+    for (int i = len - 1; i >= 0; --i) {
         rtv += bit_tree.sum(nums[i] - 1);
         bit_tree.add(nums[i], 1);
     }
--- test.cpp
+++ test.cpp
@@ -1,9 +1,29 @@
-// Leetcode 1723
 #include <algorithm>
+#include <stdio.h>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
+int countTriplets(vector<int>& arr)
+{
+    int N = arr.size();
+    unordered_map<int, int> cnt, total;
+    int ans = 0;
+    int s = 0;
+    for (int k = 0; k < N; ++k) {
+        int val = s ^ arr[k];
+        if (cnt.count(val)) {
+            ans += cnt[val] * k - total[val];
+        }
+        ++cnt[s];
+        total[s] += k;
+        s = val;
+    }
+    return ans;
+}
 int main(void)
 {
+    vector<int> arr = { 2, 3, 1, 6, 7 };
+    printf("countTriplets(arr) = %d\n", countTriplets(arr));
     return 0;
 }
