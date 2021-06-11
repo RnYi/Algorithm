2d072279889846a1703548b58a60f3824e3c8c03
rany <rany97@qq.com>
2021-06-11 17:57:43 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -1,24 +1,49 @@
-#include <algorithm>
-#include <stdint.h>
-#include <stdio.h>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <typeinfo>
+#include <utility>
 #include <vector>
 using namespace std;
 
-int hammingDistance(int x, int y)
+inline bool inBound(int m, int n, int x, int y)
 {
-    x^=y;
-    int count=0;
-    while(x){
-        x&=(x-1);
-        ++count;
+    return x >= 0 && x < m && y >= 0 && y < n;
+}
+
+bool isToeplitzMatrix(vector<vector<int>>& matrix)
+{
+    int m = matrix.size();
+    int n = matrix[0].size();
+    // 0,0
+    int sx, sy;
+    sx = m - 1;
+    sy = 0;
+    while (sy!=n) {
+        cout<<"sx: "<<sx<<'\t'<<"sy: "<<sy<<endl;
+        int val = matrix[sx][sy];
+        int x = sx, y = sy;
+        while (inBound(m, n, x, y)) {
+            if (matrix[x][y] != val) {
+                return false;
+            }
+            ++x;
+            ++y;
+        }
+        if (sx != 0) {
+            --sx;
+        } else {
+            ++sy;
+        }
     }
-    return count;
+    return true;
 }
 
 int main(void)
 {
-    int x=1;
-    int y=4;
-    printf("hammingDistance(x,y) = %d\n", hammingDistance(x,y));
+    vector<vector<int>> mat = {
+        {1,2}, {2,2}
+    };
+    printf("isToeplitzMatrix(mat) = %d\n", isToeplitzMatrix(mat));
     return 0;
 }
