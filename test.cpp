902623b3664f5fc8c9370c866547f6f82a365b64
RanY <rany97@qq.com>
2023-03-03 15:15:32 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <vector>
 
-int main(void) {
-  int x;
-  std::cin >> x;
-  std::cout << x << std::endl;
-  return 0;
+int main() {
+    std::vector<int> vec = {1,2,3,4,5};
+    for(auto& num : vec){
+        num=(num%2==0)?num:num*2;
+    }
+    for(auto& num : vec){
+        std::cout<<num<<' ';
+    }
+    return 0;
 }
