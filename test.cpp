b038fd08eec3024410c091b6a20593910c098410
RanY <rany97@qq.com>
2023-03-13 19:02:42 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
-#include <vector>
+#include <string>
+
+class Employee {
+public:
+  Employee() : id(employee_id()) {}
+  explicit Employee(const std::string &p_name)
+      : id(employee_id()), name(p_name) {}
+  Employee(const Employee &orig) = delete;
+  Employee &operator=(const Employee &rhs) = delete;
+  int get_id() const { return id; }
+  const std::string get_name() const { return name; }
+
+private:
+  static int employee_id();
+
+private:
+  int id;
+  std::string name;
+  static int seq;
+};
+int Employee::seq = 0;
+int Employee::employee_id() { return seq++; }
 
 int main() {
-    std::vector<int> vec = {1,2,3,4,5};
-    for(auto& num : vec){
-        num=(num%2==0)?num:num*2;
-    }
-    for(auto& num : vec){
-        std::cout<<num<<' ';
-    }
-    return 0;
+  Employee employees[3];
+  for (auto &e : employees) {
+    std::cout << e.get_id() << ' ';
+  }
+  const Employee a("123");
+  std::cout << a.get_id() << '\n';
+  std::cout << a.get_name() << '\n';
+  return 0;
 }
